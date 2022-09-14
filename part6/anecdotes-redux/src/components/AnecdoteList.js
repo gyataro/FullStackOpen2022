c45a5426bba0c87178fcc5350eff61f2d4d13a32
Xiwen Teoh <teoh6g@gmail.com>
2022-09-14 23:32:49 +0800

    Part 6: Many Reducers

--- part6/anecdotes-redux/src/components/AnecdoteList.js
+++ part6/anecdotes-redux/src/components/AnecdoteList.js
@@ -1,5 +1,6 @@
 import { useDispatch, useSelector } from "react-redux"
 import { voteAnecdote } from "../reducers/anecdoteReducer"
+import { showNotification, hideNotification } from "../reducers/notificationReducer"
 
 const Anecdote = ({ anecdote, handleVote }) => {
   return (
@@ -13,7 +14,17 @@ const Anecdote = ({ anecdote, handleVote }) => {
 
 const AnecdoteList = () => {
   const dispatch = useDispatch()
-  const anecdotes = useSelector(state => state)
+  const anecdotes = useSelector(state => 
+    state.anecdotes.filter((anecdote) => anecdote.content.toLowerCase().includes(state.filter))
+  )
+
+  const handleVote = (anecdote) => {
+    dispatch(voteAnecdote(anecdote.id))
+    dispatch(showNotification(`you voted '${anecdote.content}'`))
+    setTimeout(() => {
+      dispatch(hideNotification())
+    }, 5000)
+  }
 
   return (
     <ol>
@@ -21,7 +32,7 @@ const AnecdoteList = () => {
         <Anecdote
           key={anecdote.id}
           anecdote={anecdote}
-          handleVote={() => dispatch(voteAnecdote(anecdote.id))} 
+          handleVote={() => handleVote(anecdote)} 
         />
       )}
     </ol>
